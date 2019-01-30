2ebfc1357cf4c77063cffee267964fd3e36e7cd4
Cheranga Hatangala <cheranga@gmail.com>
2019-01-30 15:06:08 +1100

    Add project files.

---
+++ AppointmentFunctions/NewAppointmentHandler.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Appointments.Business.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AppointmentFunctions
+{
+    public static class NewAppointmentHandler
+    {
+        [FunctionName("NewAppointment")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
+            HttpRequest req,
+            [Queue("new-appointments")] IAsyncCollector<NewAppointmentRequest> newAppointmentsQueue,
+            ILogger log)
+        {
+            log.LogInformation($"{nameof(NewAppointmentHandler)} called");
+
+            var content = await new StreamReader(req.Body).ReadToEndAsync().ConfigureAwait(false);
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                return new BadRequestResult();
+            }
+            try
+            {
+                var newAppointmentRequest = JsonConvert.DeserializeObject<NewAppointmentRequest>(content);
+                var isValid = newAppointmentRequest != null && newAppointmentRequest.IsValid();
+
+                if (isValid)
+                {
+                    await newAppointmentsQueue.AddAsync(newAppointmentRequest).ConfigureAwait(false);
+
+                    log.LogInformation($"New appointmentment created for {newAppointmentRequest.Name} :: {newAppointmentRequest.Email} :: {newAppointmentRequest.Number}");
+                    return new OkResult();
+                }
+
+                return new BadRequestObjectResult(newAppointmentRequest);
+            }
+            catch (Exception exception)
+            {
+                log.LogError(exception, "Cannot add a new appointment");
+                return new InternalServerErrorResult();
+            }
+        }
+    }
+}
---
+++ Appointments.Business/DTO/NewAppointmentRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Appointments.Business.DTO
+{
+    public class NewAppointmentRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Number { get; set; }
+
+        public bool IsValid() => !string.IsNullOrWhiteSpace(Name) && !string.IsNullOrWhiteSpace(Email) && !string.IsNullOrWhiteSpace(Number);
+    }
+}
