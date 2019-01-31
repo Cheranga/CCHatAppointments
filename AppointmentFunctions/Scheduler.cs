cb7384d1e92848fb18728f70a41a25f82789d18e
Cheranga Hatangala <cheranga@gmail.com>
2019-01-31 21:39:28 +1100

    Adding IoC to the "Scheduler" azure function

--- AppointmentFunctions/Scheduler.cs
+++ AppointmentFunctions/Scheduler.cs
@@ -1,13 +1,21 @@
+using Appointments.Business;
+using Appointments.Business.Models;
+using AzureFunctions.Autofac;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
 namespace AppointmentFunctions
 {
+    [DependencyInjectionConfig(typeof(IoCConfig))]
     public static class Scheduler
     {
         [FunctionName("Scheduler")]
-        public static void Run([QueueTrigger("new-appointments", Connection = "")]
-            string myQueueItem, ILogger log)
+        public static void Run(
+            [QueueTrigger("new-appointments", Connection = "")]
+            string myQueueItem,
+            [Inject]
+            IAppointmentDecider<NextAvailableAppointment> decider,
+            ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
