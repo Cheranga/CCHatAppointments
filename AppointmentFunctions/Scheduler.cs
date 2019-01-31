8254e1bdfe0c2e35fd83734bb2ac83d8b248d92d
Cheranga Hatangala <cheranga@gmail.com>
2019-01-31 21:48:16 +1100

    Depending on the availability the appointment will be sent to the appropriate blob container.

--- AppointmentFunctions/Scheduler.cs
+++ AppointmentFunctions/Scheduler.cs
@@ -1,8 +1,11 @@
+using System;
 using Appointments.Business;
+using Appointments.Business.DTO;
 using Appointments.Business.Models;
 using AzureFunctions.Autofac;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace AppointmentFunctions
 {
@@ -11,13 +14,35 @@ namespace AppointmentFunctions
     {
         [FunctionName("Scheduler")]
         public static void Run(
-            [QueueTrigger("new-appointments", Connection = "")]
-            string myQueueItem,
-            [Inject]
-            IAppointmentDecider<NextAvailableAppointment> decider,
+            [QueueTrigger("new-appointments", Connection = "")]NewAppointmentRequest newAppointment,
+            [Blob("scheduled-appointments/{rand-guid}")]out string scheduledAppointment,
+            [Blob("rejected-requests/{rand-guid}")]out string rejectedAppointment,
+            [Inject]IAppointmentDecider<NextAvailableAppointment> decider,
             ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            log.LogInformation($"{nameof(Scheduler)} started");
+
+            scheduledAppointment = null;
+            rejectedAppointment = null;
+
+            var nextAvailableAppointment = new NextAvailableAppointment
+            {
+                Email = newAppointment.Email,
+                From = DateTime.UtcNow,
+                To = DateTime.UtcNow.AddHours(1)
+            };
+
+            var isAvailable = decider.IsAvailable(nextAvailableAppointment);
+            if (isAvailable)
+            {
+                log.LogInformation($"Appointment is scheduled for {nextAvailableAppointment.Email}");
+                scheduledAppointment = JsonConvert.SerializeObject(nextAvailableAppointment);
+            }
+            else
+            {
+                log.LogInformation($"Appointment is rejected for {nextAvailableAppointment.Email}");
+                rejectedAppointment = JsonConvert.SerializeObject(newAppointment);
+            }
         }
     }
 }
